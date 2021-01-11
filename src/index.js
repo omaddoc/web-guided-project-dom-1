f41a66ef634bb1e2797ee5d10d17f1fdd5c043e2
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-11 11:54:28 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const header = document.querySelector("header");
 const logoTitle = document.querySelector("#logoTitle");
 const firstCard = document.querySelector(".card");
 // B- finding within one particular element
-const imageFirstCard = null;
+const imageFirstCard = firstCard.querySelector("img");
 const titleFirstCard = null;
 const subtitleFirstCard = null;
 const textFirstCard = null;
