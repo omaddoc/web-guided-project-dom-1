fd471ac45acd87e0b17bbb53cb0bd875acadea47
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-11 13:00:39 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -68,6 +68,7 @@ document.querySelector("nav").appendChild(blogLink);
 // 👉 7- Making a copy of the card and appending it to the card group
 // DOM nodes can only exist in one spot in the DOM
 // We cannot append the same copy multiple times
+const secondCard = firstCard.cloneNode(true);
 
 // 👉 8- Removing an existing element and putting it back [STRETCH if time allows]
 
